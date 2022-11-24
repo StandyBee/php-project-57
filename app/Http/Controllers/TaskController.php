f897715f61541272278a41323b9b7b3b4e3e9c9d
fisak <afisak94@gmail.com>
2022-11-24 08:17:03 +0300

    add TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,82 +5,71 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $tasks = Task::orderBy('id')->get();
+        return view('tasks.index', compact('tasks'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        $task = new Task();
+        return view('tasks.create', compact('task'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreTaskRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreTaskRequest $request)
     {
-        //
+        $inputData = $this->validate($request, [
+            'name' => 'required|max:255|unique:tasks',
+            'status_id' => 'required',
+            'description' => 'nullable|string',
+            'assigned_to_id' => 'nullable|integer'
+        ]);
+
+        $user = Auth::user();
+        $task = $user->tasks()->make();
+        $task->fill($inputData);
+        $task->save();
+        
+        flash('success')->succcess();
+        return redirect()->route('tasks.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function show(Task $task)
     {
-        //
+        return view('tasks.show', compact('task'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Task $task)
     {
-        //
+        return view('tasks.edit', compact('task'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateTaskRequest  $request
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $inputData = $this->validate($request, [
+            'name' => 'required|max:255|unique:tasks',
+            'status_id' => 'required',
+            'description' => 'nullable|string',
+            'assigned_to_id' => 'nullable|integer'
+        ]);
+
+        $task->fill($inputData);
+        $task->save();
+
+        flash('succ')->success();
+        return redirect()->route('tasks.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        flash('Deleted succcess')->success();
+        return redirect()->route('tasks.index');
     }
 }
